925b0b49131e961346be7d5161e18e949bfaef9a
taylorm-portfolio <153951705+taylorm-portfolio@users.noreply.github.com>
2024-03-11 17:31:50 -0500

    Updated my_app
    added more charts, and changed the way the data is pulled from the database and formatted it with pandas pivot_table

--- my_app.py
+++ my_app.py
@@ -8,20 +8,46 @@ import sqlite3
 
 con = sqlite3.connect('sudokustats.db')
 
-df1 = pd.read_sql_query('SELECT removedCells, count(runID), round(avg(runCountSolution),2), round(avg(runCountPuzzle),2), runDate FROM runs WHERE runDate = \'2024-03-11\' GROUP BY removedCells',con)
-#df2 = pd.read_sql_query('SELECT removedCells, count(runId)')
+df = pd.read_sql_query('SELECT runID, runDate, runCountSolution, runTimeSolution, runCountPuzzle, runTimePuzzle, runTimeOverall, removedCells FROM runs WHERE runDate = \'2024-03-11\'',con)
 renameDiary = {
+    'runID': 'Run ID',
+    'runDate': 'Date Generator Run',
+    'runCountSolution': 'Runs to Generate Solution',
+    'runTimeSolution': 'Time (ms) to Generate Solution',
+    'runCountPuzzle': 'Runs to Generate Puzzle',
+    'runTimePuzzle': 'Time (ms) to Generate Puzzle',
+    'runTimeOverall': 'Time (ms) to Run Entire Generator',
     'removedCells': 'Qty Removed Cells',
     'count(runID)': '# of Runs',
     'round(avg(runCountSolution),2)': 'Avg # of Runs to Generate Solution',
     'round(avg(runCountPuzzle),2)': 'Avg # of Runs to Generate Puzzle',
-    'runDate': 'Date Generator Run'
+    
 }
-#runDates = list(df1['runDate'])
-#print(runDates)
-#dateOptions=[]
-#[dateOptions.append(x) for x in runDates if x not in dateOptions]
-#print(dateOptions)
+
+dfPivot = pd.pivot_table(
+    df, index='removedCells',
+    values=[
+        'runCountSolution',
+        'runCountPuzzle',
+        'runTimePuzzle',
+        'runTimeSolution',
+        'runTimeOverall'
+        ],
+    aggfunc={
+        'runCountSolution':'mean',
+        'runCountPuzzle': 'mean',
+        'runTimePuzzle': 'mean',
+        'runTimeSolution': 'mean',
+        'runTimeOverall': 'mean'
+        }
+    ).reset_index(drop = False).round({
+        'runCountSolution': 2,
+        'runCountPuzzle': 2,
+        'runTimePuzzle': 3,
+        'runTimeSolution': 3,
+        'runTimeOverall': 3
+    })
+print(dfPivot)
 
 # Initialize the app
 app = Dash(__name__)
@@ -35,27 +61,34 @@ app.layout = html.Div([
     #dcc.Dropdown(['New York City', 'Montréal', 'San Francisco'], 'Montréal',multi=True),
     dcc.RangeSlider(1,64,5,value=[1,64],id='page_slider'),
     #dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
-    dcc.Graph(figure={},id='puzzle_runs_graph'),# id='controls-and-graph')
-    dcc.Graph(figure={},id='sln_runs_graph'),# id='controls-and-graph')
-    dash_table.DataTable(data=df1.to_dict('records'), columns=[{"name": renameDiary[i], "id": i} for i in df1.columns], page_size=10, id='data_table',
+    dcc.Graph(figure={},id='puzzle_runs_graph'),
+    dcc.Graph(figure={},id='sln_runs_graph'),
+    dcc.Graph(figure={},id='puzzle_time_graph'),
+    dcc.Graph(figure={},id='sln_time_graph'),
+    dash_table.DataTable(data=dfPivot.to_dict('records'), columns=[{"name": renameDiary[i], "id": i} for i in dfPivot.columns], page_size=10, id='data_table',
         filter_action="native")
+    
 ])
 
 @app.callback(
     Output(component_id='data_table', component_property='data'),
-    Output(component_id='sln_runs_graph',component_property='figure'),
     Output(component_id='puzzle_runs_graph',component_property='figure'),
+    Output(component_id='sln_runs_graph',component_property='figure'),
+    Output(component_id='puzzle_time_graph',component_property='figure'),
+    Output(component_id='sln_time_graph',component_property='figure'),
     Input(component_id='page_slider',component_property='value')
     #Input()
 )
 
 def update_table(slider_range):
     # Filter the DataFrame based on the slider range
-    updated_data = df1[df1['removedCells'].between(slider_range[0], slider_range[1])]
+    updated_data = dfPivot[dfPivot['removedCells'].between(slider_range[0], slider_range[1])]
     # Create the figure using the filtered DataFrame
-    fig1 = px.bar(updated_data,x='removedCells',y='round(avg(runCountSolution),2)',labels=renameDiary)
-    fig2 = px.bar(updated_data,x='removedCells',y='round(avg(runCountPuzzle),2)', labels=renameDiary)
-    return updated_data.to_dict('records'), fig1, fig2
+    fig1 = px.bar(updated_data,x='removedCells',y='runCountPuzzle', labels=renameDiary, title='Average Loop Runs to Generate Puzzle')
+    fig2 = px.bar(updated_data,x='removedCells',y='runCountSolution',labels=renameDiary, title='Average Loop Runs to Generate Solution')
+    fig3 = px.bar(updated_data,x='removedCells',y='runTimePuzzle', labels=renameDiary, title='Average Loop Completion Time to Generate Puzzle')
+    fig4 = px.bar(updated_data,x='removedCells',y='runTimeSolution', labels=renameDiary, title='Average Loop Completion Time to Generate Solution')
+    return updated_data.to_dict('records'), fig1, fig2, fig3, fig4
 # Add controls to build the interaction
 
 #def update_graph(slider_range):
--- runCode.py
+++ runCode.py
@@ -5,10 +5,10 @@ from solver import backtrackSolver
 import pandas as pd
 lim = 50
 
-for i in range(10,lim):
-    print(f"running funciton for i = {i}")
-    for k in range(1,150):
-        sudokuGen(i)
+#for i in range(10,lim):
+#    print(f"running funciton for i = {i}")
+#    for k in range(1,150):
+#        sudokuGen(i)
 #sudokuGen(64)
 
 #mylist = retrieveData('''SELECT removedCells,count(removedCells) FROM runs GROUP BY removedCells''',2)
@@ -31,3 +31,34 @@ board = [
 #df = pd.DataFrame(board)
 #print(df)
 #df.to_csv('puzzlesolved.csv', index=False)
+
+# Import packages
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import pandas as pd
+import plotly.express as px
+import sqlite3
+
+# Incorporate data
+
+con = sqlite3.connect('sudokustats.db')
+
+df1 = pd.read_sql_query('SELECT removedCells, count(runID), round(avg(runCountSolution),2), round(avg(runCountPuzzle),2), runDate FROM runs WHERE runDate = \'2024-03-11\' GROUP BY removedCells',con)
+#df2 = pd.read_sql_query('SELECT removedCells, count(runId)')
+df2 = pd.read_sql_query('SELECT runID, runDate, runCountSolution, runTimeSolution, runCountPuzzle, runTimePuzzle, runTimeOverall, removedCells FROM runs WHERE runDate = \'2024-03-11\'',con)
+renameDiary = {
+    'runID': 'Run ID',
+    'runDate': 'Date Generator Run',
+    'runCountSolution': 'Runs to Generate Solution',
+    'runTimeSolution': 'Time (ms) to Generate Solution',
+    'runCountPuzzle': 'Runs to Generate Puzzle',
+    'runTimePuzzle': 'Time (ms) to Generate Puzzle',
+    'runTimeOverall': 'Time (ms) to Run Entire Generator',
+    'removedCells': 'Qty Removed Cells',
+    'count(runID)': '# of Runs',
+    'round(avg(runCountSolution),2)': 'Avg # of Runs to Generate Solution',
+    'round(avg(runCountPuzzle),2)': 'Avg # of Runs to Generate Puzzle',
+    
+}
+
+dfPivot = pd.pivot_table(df2, index='removedCells', values=['runCountSolution','runCountPuzzle'], aggfunc={'runCountSolution':'mean','runCountPuzzle': 'mean'}).reset_index(drop = False)
+print(dfPivot)
